4638ac1a36371861270b280a9989f423181e3c73
hypercyte <hypercyte@gmail.com>
2022-02-07 18:44:11 +0000

    express server to serve content

--- server/server.js
+++ server/server.js
@@ -13,3 +13,9 @@ const port          = process.env.PORT || 3000;
 let app             = express();
 let server          = http.createServer(app);
 let io              = socketIO(server);
+
+app.use(express.static(publicPath));
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}.`)
+});
